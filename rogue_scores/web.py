d6f4a857bd61b1f17c789f8c81f418b1e4c7d70a
Baptiste Fontaine <batifon@yahoo.fr>
2014-05-26 21:38:40 +0200

    improved error handling

--- rogue_scores/web.py
+++ rogue_scores/web.py
@@ -24,35 +24,39 @@ def init_scores():
 
 def sanitize_score(sc):
     """
-    Sanitize a score from an external source
+    Sanitize a score from an external source. Its argument should be a tuple of
+    ``user``, ``score``, and ``text``.
     """
     u, s, t = sc[:3]
 
     try:
-        s = str(int(s))
+        s = int(s)
     except ValueError:
         s = 0
+    except TypeError:
+        s = 0
 
-    u = re.sub(r'\W+', '', u.strip())[:40]
-    t = re.sub(r'[^\w\., ]+', '', t)[:80]
+    u = re.sub(r'\W+', '', str(u).strip())[:40]
+    t = re.sub(r'[^\w\., ]+', '', str(t))[:80]
     return (u, s, t)
 
 def sanitize_scores(scs):
     """
-    Sanitize scores from an external source
+    Sanitize scores from an external source, and filter out those with an empty
+    username and a null score.
     """
-    return list(map(sanitize_score, scs))
+    return [s for s in map(sanitize_score, scs) if s[0] and s[1]>0]
 
 def merge_scores(scs):
     """
     Merge local scores with the given ones, and save the new list in the local
     file.
     """
+    scs = sanitize_scores(scs)
+
     init_scores()
     with open(app.config['SCORES'], 'r') as f:
         scores = json.loads(f.read())
-
-    scs = [list(map(str, s)) for s in scs]
 
     final_scores = []
     while scores or scs:
@@ -67,16 +71,12 @@ def merge_scores(scs):
         s1 = scores[0]
         s2 = scs[0]
 
-        if int(s1[1]) <= 0:
-            scores.pop(0)
-            continue
-
-        if int(s2[1]) <= 0 or s1 == s2:
-            # don't add duplicates or null scores in the file
+        if s1 == s2:
+            # don't add duplicates in the file
             scs.pop(0)
             continue
 
-        coll = scores if int(s1[1]) > int(s2[1]) else scs
+        coll = scores if s1[1] > s2[1] else scs
         final_scores.append(coll.pop(0))
 
     with open(app.config['SCORES'], 'w') as f:
@@ -92,7 +92,10 @@ def index():
 
 @app.route('/scores', methods=['POST'])
 def scores_upload():
-    scores = sanitize_scores(json.loads(request.form['scores']))
-    r = merge_scores(scores)
+    try:
+        scores = json.loads(request.form['scores'])
+    except ValueError:
+        return 'wrong json'
+    merge_scores(scores)
     return 'ok'
 
