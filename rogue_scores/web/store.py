476161ec590ef2d61891cb238afdf0edc2ab0f08
Baptiste Fontaine <batifon@yahoo.fr>
2014-05-30 15:21:50 +0200

    ScoresStore#_insert implemented

--- rogue_scores/web/store.py
+++ rogue_scores/web/store.py
@@ -8,16 +8,24 @@ import os.path
 import re
 import json
 
+def parse_text(text):
+    """
+    Parse a score's text and return a dictionnary
+    """
+    # TODO
+
+    # ^killed, ^died, ^quit
+    # on level (\d+)
+    # of ([a-z]+), by an? ([a-z]+), by ([a-z]+)
+
+    return {}
+
+
 class Score(object):
     def __init__(self, **kwargs):
         self.level = self.score = 0
-        self.user = self.cause = self.monster = None
+        self.user = self.cause = self.status = None
         self.__dict__.update(kwargs)
-
-    def __int__(self):
-        return self.level
-
-    def __eq__(self):
 
     def json(self):
         return json.dumps(self.__dict__)
@@ -28,6 +36,31 @@ class Score(object):
     def load(data):
         ls = json.loads(data)
         return list(map(Score, ls))
+
+    def __int__(self):
+        return self.score
+
+    def __eq__(self, o):
+        if not isinstance(o, Score):
+            return False
+
+        for attr in ('level', 'score', 'user', 'cause', 'status'):
+            if getattr(self, attr) != getattr(o, attr):
+                return False
+
+        return True
+
+    def __gt__(self, o):
+        return int(self) > int(o)
+
+    def __ge__(self, o):
+        return int(self) >= int(o)
+
+    def __lt__(self, o):
+        return int(self) < int(o)
+
+    def __le__(self, o):
+        return int(self) <= int(o)
 
 
 class ScoresStore(object):
@@ -66,17 +99,16 @@ class ScoresStore(object):
         """
         Insert a score.
         """
-        # TODO: we need to insert at the right place, and return False if
-        # the score is already there
-
-        idx = 1
-        for i,s1 in enumerate(self.scores):
-            idx = i+1
-            if s1.score < s.score:
+        for i, s1 in enumerate(self.scores):
+            if s == s1:
+                return False
+            if s > s1:
+                self.scores.insert(i, s)
                 break
-
-        self.scores.insert(idx, s)
-        self.saved = False
+        else:
+            self.saved = False
+            self.scores.append(s)
+            return True
 
     def _add(self, s, **attrs):
         """
@@ -86,8 +118,8 @@ class ScoresStore(object):
         attrs.update(dict(s))
 
         if 'cause' not in attrs and 'text' in attrs:
-            attrs['status'], attrs['level'], attrs['cause'] = \
-                parse_text(attrs['text'])
+            attrs.update(parse_text(attrs['text']))
+            del attrs['text']
 
         # sanitize the score & level
         try:
@@ -115,6 +147,10 @@ class ScoresStore(object):
         """
         ct = 0
         for s in scs:
+            if isinstance(s, tule) or isinstance(s, list):
+                # old format
+                s = {'user': s[0], 'score': s[1], 'text': s[2]}
+
             if self._add(s, **kwargs):
                 ct += 1
 
@@ -123,4 +159,3 @@ class ScoresStore(object):
     def __del__(self):
         if not self.saved:
             self.save()
-
